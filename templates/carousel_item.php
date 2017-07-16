73ebb752f1dbfc90c239eb0da0e873a7b635858e
Irune Itoiz <irune.itoiz@gmail.com>
2017-07-16 18:10:36 +0100

    Added meta data to link for tracking on Analytics

--- templates/carousel_item.php
+++ templates/carousel_item.php
@@ -1,5 +1,5 @@
 <li class="amazon-carousel-item">
-    <a href="<?php echo $item['product_link']; ?>">
+    <a href="<?php echo $item['product_link']; ?>" data-vars-outbound-link="Carousel <?php echo $item['product_name']; ?>">
         <img src="<?php echo $item['product_image']['sizes']['amazon_thumbnail']; ?>">
         <p><?php echo $item['product_name']; ?></p>
         <img src="<?php echo plugins_url('assets/buy_now.gif', dirname(__FILE__)); ?>">
--- templates/single_item.php
+++ templates/single_item.php
@@ -1,5 +1,5 @@
 <div class="amazon-item">
-    <a href="<?php echo $item['product_link']; ?>">
+    <a href="<?php echo $item['product_link']; ?>" data-vars-outbound-link="Single Item <?php echo $item['product_name']; ?>">
         <img src="<?php echo $item['product_image']['sizes']['amazon_thumbnail']; ?>">
         <p><?php echo $item['product_name']; ?></p>
         <img src="<?php echo plugins_url('assets/buy_now.gif', dirname(__FILE__)); ?>">
